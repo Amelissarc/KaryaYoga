a53ac1d430f4b06e46c1604bf06a6ff0c47898aa
Amelissarc <melissa.rinconft@gmail.com>
2023-06-16 19:34:32 -0300

    loginPage msj confirmación

--- src/components/header/LoginWidget/LoginPage.jsx
+++ src/components/header/LoginWidget/LoginPage.jsx
@@ -4,51 +4,87 @@ import Form from 'react-bootstrap/Form';
 import Styles from './LoginPage.css';
 
 export const LoginWidget = ({ onLogin }) => {
-const [showForm, setShowForm] = useState(false);
-const titleName = 'Sign in';
+  const [showForm, setShowForm] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const titleName = 'Sign in';
 
-const handleLoginClick = () => {
+  const handleLoginClick = () => {
     setShowForm(true);
-};
+  };
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // Perform login 
+    // Realiza el inicio de sesión con los datos del usuario
+
+    // Simulación de inicio de sesión exitoso
+    setIsLoggedIn(true);
+
+    // Mostrar mensaje de aviso
+    alert('Inicio de sesión exitoso');
+
     onLogin();
-};
+  };
 
-if (!showForm) {
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setEmail('');
+    setPassword('');
+  };
+
+  if (isLoggedIn) {
     return (
-        <div className="loginWidget" onClick={handleLoginClick}>
-            <i className="fa-regular fa-user"></i>
-            <span>{titleName}</span>
-        </div>
+      <div className="loginWidget" onClick={handleLogout}>
+        <i className="fa-regular fa-user"></i>
+        <span>Logged in</span>
+      </div>
     );
-}
+  }
 
+  if (!showForm) {
     return (
-    <Form onSubmit={handleSubmit} className='loginPage'>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
+      <div className="loginWidget" onClick={handleLoginClick}>
+        <i className="fa-regular fa-user"></i>
+        <span>{titleName}</span>
+      </div>
+    );
+  }
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+  return (
+    <Form onSubmit={handleSubmit} className="loginPage">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
 
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
     </Form>
-    );
+  );
 };
+
 export default LoginWidget;
